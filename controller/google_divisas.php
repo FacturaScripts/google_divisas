d455e1a6e528058d7c8fa5ca0010c50762dd9ee2
NeoRazorX <neorazorx@gmail.com>
2018-06-29 11:37:40 +0200

    - Cambiada la API de google por la de free.currencyconverterapi.com. - La de google fue cerrada hace unas semanas.

--- controller/google_divisas.php
+++ controller/google_divisas.php
@@ -74,92 +74,15 @@ class google_divisas extends fs_controller
 
     private function convert_currency($amount, $from, $to)
     {
-        $url = "https://finance.google.com/finance/converter?a=" . $amount . "&from=" . $from . "&to=" . $to;
-        $data = $this->curl_get_contents($url);
-        $converted = array();
-        preg_match("/<span class=bld>(.*)<\/span>/", $data, $converted);
+        $url = "http://free.currencyconverterapi.com/api/v5/convert?q=" . $from . "_" . $to . "&compact=ultra";
+        $data = fs_file_get_contents($url);
+        $json = json_decode($data, true);
 
-        if (count($converted) > 0) {
-            return floatval($converted[1]);
+        $tasa = 1;
+        if (isset($json[$from . '_' . $to])) {
+            $tasa = (float) $json[$from . '_' . $to];
         }
 
-        return 1;
-    }
-
-    /**
-     * Descarga el contenido con curl o file_get_contents
-     * @param type $url
-     * @param type $timeout
-     * @return type
-     */
-    private function curl_get_contents($url)
-    {
-        if (function_exists('curl_init')) {
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-            if (defined('FS_PROXY_TYPE')) {
-                curl_setopt($ch, CURLOPT_PROXYTYPE, FS_PROXY_TYPE);
-                curl_setopt($ch, CURLOPT_PROXY, FS_PROXY_HOST);
-                curl_setopt($ch, CURLOPT_PROXYPORT, FS_PROXY_PORT);
-            }
-            $data = curl_exec($ch);
-            $info = curl_getinfo($ch);
-
-            if ($info['http_code'] == 301 OR $info['http_code'] == 302) {
-                $redirs = 0;
-                return $this->curl_redirect_exec($ch, $redirs);
-            }
-
-            curl_close($ch);
-            return $data;
-        }
-
-        return file_get_contents($url);
-    }
-
-    /**
-     * Función alternativa para cuando el followlocation falla.
-     * @param type $ch
-     * @param type $redirects
-     * @param type $curlopt_header
-     * @return type
-     */
-    private function curl_redirect_exec($ch, &$redirects, $curlopt_header = false)
-    {
-        curl_setopt($ch, CURLOPT_HEADER, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        if (defined('FS_PROXY_TYPE')) {
-            curl_setopt($ch, CURLOPT_PROXYTYPE, FS_PROXY_TYPE);
-            curl_setopt($ch, CURLOPT_PROXY, FS_PROXY_HOST);
-            curl_setopt($ch, CURLOPT_PROXYPORT, FS_PROXY_PORT);
-        }
-        $data = curl_exec($ch);
-        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-        if ($http_code == 301 || $http_code == 302) {
-            list($header) = explode("\r\n\r\n", $data, 2);
-            $matches = array();
-            preg_match("/(Location:|URI:)[^(\n)]*/", $header, $matches);
-            $url = trim(str_replace($matches[1], "", $matches[0]));
-            $url_parsed = parse_url($url);
-            if (isset($url_parsed)) {
-                curl_setopt($ch, CURLOPT_URL, $url);
-                $redirects++;
-                return $this->curl_redirect_exec($ch, $redirects, $curlopt_header);
-            }
-        }
-
-        if ($curlopt_header) {
-            curl_close($ch);
-            return $data;
-        }
-
-        list(, $body) = explode("\r\n\r\n", $data, 2);
-        curl_close($ch);
-        return $body;
+        return $amount * $tasa;
     }
 }
